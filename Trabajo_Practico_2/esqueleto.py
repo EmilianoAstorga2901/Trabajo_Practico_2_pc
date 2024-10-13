c6058bf06e1738fd0795d85ec63093f91e177f4b
Emiliano Astorga <eastorga@udesa.edu.ar>
2024-10-13 12:46:15 -0300

    agregue generador_matrices_filter a funciones

--- Trabajo_Practico_2/esqueleto.py
+++ Trabajo_Practico_2/esqueleto.py
@@ -1,4 +1,5 @@
 import funciones_tp2
+import numpy as np
 foto1='fotos/beach.jpg'
 foto2='fotos/bench.jpg'
 foto3='fotos/castle.jpg'
@@ -7,3 +8,5 @@ foto5='fotos/dali2.jpg'
 foto6='fotos/dolphin.jpg'
 foto7='fotos/lake.jpg'
 Matriz=funciones_tp2.imagen_a_matriz(foto2)
+
+filter=funciones_tp2.generador_matrices_filter(np.array([[1,2,0], [0,0,2],[0,2,2]]))
--- Trabajo_Practico_2/funciones_tp2.py
+++ Trabajo_Practico_2/funciones_tp2.py
@@ -12,3 +12,31 @@ def imagen_a_matriz(foto:str)->int:
     print(imagen)
     matriz=np.array(imagen)
     print(matriz)
+
+
+def generador_matrices_filter (matriz:int)-> int:
+    """ 
+    Funcion que se encarga de generar matrices 
+    Parametros:
+        matriz:int
+    Reetorna:
+    """ 
+    print(type(matriz))
+    print(matriz)
+
+def padding (matriz:int,filter:int):
+    
+    filter=np.array(x,y,z)
+    print(filter)
+    matriz_padding=str(matriz) 
+    
+
+
+
+
+
+
+
+
+#def convolucion (input:int , filter: int) ->int :
+    
