73ccd82e6bd6e01ac658316715abbc7863dfd15c
Emiliano Astorga <eastorga@udesa.edu.ar>
2024-10-20 18:22:50 -0300

    Tp 2 ultima actualizacion

--- Trabajo_Practico_2/esqueleto.py
+++ Trabajo_Practico_2/esqueleto.py
@@ -1,23 +1,92 @@
-import funciones_tp2
-import numpy as np
 import PIL
+from PIL import ImageTk
+import funciones_tp2
+import numpy as np 
+import tkinter as TK
+
 links=('fotos/beach.jpg','fotos/bench.jpg','fotos/castle.jpg', 'fotos/dali.jpg', 
        'fotos/dali2.jpg', 'fotos/dolphin.jpg', 'fotos/lake.jpg', 'fotos/messi.webp')
-Imagen_RGB=input('Ingrese el link de la foto:')
-Matriz=funciones_tp2.imagen_a_matriz(Imagen_RGB)
-hacer_padding= funciones_tp2.padding(Matriz)
-print(hacer_padding)
-print(Matriz.shape)
-print(hacer_padding.shape)
-hacer_padding_r = hacer_padding[:,:,0]
-hacer_padding_g = hacer_padding[:,:,1]
-hacer_padding_b = hacer_padding[:,:,2] 
-sobel_x_r, sobel_y_r = funciones_tp2.aplicar_sobel(hacer_padding_r)
-sobel_x_g, sobel_y_g = funciones_tp2.aplicar_sobel(hacer_padding_g)
-sobel_x_b, sobel_y_b = funciones_tp2.aplicar_sobel(hacer_padding_b)
-sobel_x_promedio = (sobel_x_r + sobel_x_g + sobel_x_b) / 3
-sobel_y_promedio = (sobel_y_r + sobel_y_g + sobel_y_b) / 3
-gradiente=funciones_tp2.caclular_gradiente(sobel_x_promedio,sobel_y_promedio)
-matriz_energia=funciones_tp2.matriz_energia(gradiente)
-Imagen_matriz_energia=funciones_tp2.mostrar_imagen_final(matriz_energia)
+
+def main():
+
+    Imagen_RGB= input('Ingrese el link de la foto: ')
+    if Imagen_RGB not in links:
+        print('No se encontro la imagen...')
+        return
+    Matriz=funciones_tp2.imagen_a_matriz(Imagen_RGB)
+    print(f'Las dimensiones originales son {Matriz.shape[0]} x {Matriz.shape[1]}')
+    nuevo_ancho = int(input("Ingrese un nuevo ancho: "))
+    nuevo_alto = int(input("Ingrese un nuevo largo: "))
+    if nuevo_ancho < 0 or nuevo_alto < 0 or nuevo_ancho > Matriz.shape[0] or nuevo_alto > Matriz.shape[1]:
+        print('El ancho o alto no pueden ser negativos ni mayor a su ancho o alto original')
+        return
+
+    Matrices=[]
+    
+    
+    while Matriz.shape[1] > nuevo_ancho:
+
+        hacer_padding= funciones_tp2.padding(Matriz)
+
+        hacer_padding_r = hacer_padding[:,:,0]
+        hacer_padding_g = hacer_padding[:,:,1]
+        hacer_padding_b = hacer_padding[:,:,2] 
+
+        sobel_x_r, sobel_y_r = funciones_tp2.aplicar_sobel(hacer_padding_r)
+        sobel_x_g, sobel_y_g = funciones_tp2.aplicar_sobel(hacer_padding_g)
+        sobel_x_b, sobel_y_b = funciones_tp2.aplicar_sobel(hacer_padding_b)
+
+        sobel_x_promedio = (sobel_x_r + sobel_x_g + sobel_x_b) / 3
+        sobel_y_promedio = (sobel_y_r + sobel_y_g + sobel_y_b) / 3
+
+        gradiente=funciones_tp2.caclular_gradiente(sobel_x_promedio,sobel_y_promedio)
+
+        matriz_energia_acumulada = funciones_tp2.matriz_energia_acumulada_columnas(gradiente)
+
+
+        costura_columnas=funciones_tp2.costura_de_menor_energia_columnas(matriz_energia_acumulada)
+
+        eliminar_columnas=funciones_tp2.eliminar_costuras_columnas(Matriz , costura_columnas)
+
+        Matriz = eliminar_columnas
+        Matrices.append(Matriz)
+
+
+    while Matriz.shape[0] > nuevo_alto:
+
+        hacer_padding= funciones_tp2.padding(Matriz)
+
+        hacer_padding_r = hacer_padding[:,:,0]
+        hacer_padding_g = hacer_padding[:,:,1]
+        hacer_padding_b = hacer_padding[:,:,2] 
+
+        sobel_x_r, sobel_y_r = funciones_tp2.aplicar_sobel(hacer_padding_r)
+        sobel_x_g, sobel_y_g = funciones_tp2.aplicar_sobel(hacer_padding_g)
+        sobel_x_b, sobel_y_b = funciones_tp2.aplicar_sobel(hacer_padding_b)
+
+        sobel_x_promedio = (sobel_x_r + sobel_x_g + sobel_x_b) / 3
+        sobel_y_promedio = (sobel_y_r + sobel_y_g + sobel_y_b) / 3
+
+        gradiente=funciones_tp2.caclular_gradiente(sobel_x_promedio,sobel_y_promedio)
+
+        matriz_energia_acumulada = funciones_tp2.matriz_energia_acumulada(gradiente)
+
+        costura_filas=funciones_tp2.costura_de_menor_energia_filas(matriz_energia_acumulada)
+
+        eliminar_filas=funciones_tp2.eliminar_costuras_filas(Matriz, costura_filas)
+        Matriz = eliminar_filas
+        Matrices.append(Matriz)
+
+
+    
+    ventana= TK.Tk()
+    imagen_visible=TK.Label(ventana,image=PIL.ImageTk.PhotoImage(PIL.Image.fromarray(Matrices[0])))
+    imagen_visible.pack()
+    slider = TK.Scale(ventana, from_=0, to=len(Matrices)-1, orient='horizontal', command=lambda texto_slider: funciones_tp2.callback(imagen_visible, PIL.ImageTk.PhotoImage(PIL.Image.fromarray(Matrices[int(texto_slider)]))))
+    slider.pack()
+    ventana.mainloop()
+
+
+if __name__ == "__main__":
+    main()
 
--- Trabajo_Practico_2/funciones_tp2.py
+++ Trabajo_Practico_2/funciones_tp2.py
@@ -1,44 +1,22 @@
 import numpy as np 
-
 from PIL import Image
 
 def imagen_a_matriz(foto:str)->int:
     """
-
     Funcion que transforma imagenes en mateices:
-
     Parametros:
         foto:str
         matriz:int
-
     Retorna:
     """ 
-
     imagen=Image.open(foto)
-    print(imagen)
 
     matriz=np.array(imagen)
-    print(matriz)
+    
     return matriz
 
 
-
-
-def generador_matrices_filter (matriz:int)-> int:
-    """ 
-
-    Funcion que se encarga de generar matrices filtros
-
-    Parametros:
-        matriz:int
-
-    Reetorna:
-    """ 
-
-    (type(matriz))
-    print(matriz)
-
-def padding (Matriz_rgb):
+def padding (Matriz_rgb:np.ndarray)->np.ndarray:
     """ 
     Funcion que crea una matriz de ceros y pega la matriz de imagen adentro de la de ceros 
     Parametros:
@@ -46,10 +24,11 @@ def padding (Matriz_rgb):
     Retorna:
         matriz_padding:Matriz rodeada de ceros
     """
-    alto,ancho,canales = Matriz_rgb.shape #shape nos da 3 valores y nosotros asignamos esos valores a largo ancho y canales
-    matriz_padding= np.zeros((alto + 2 , ancho + 2 , canales ),dtype=Matriz_rgb.dtype) # crea una  matriz de ceros con 2 filas y columnas mas que la oiginal 
-    matriz_padding[1:alto + 1, 1:ancho + 1, :] = Matriz_rgb #copiamos la matriz original en el centro de la matriz de ceros
-    print('Matriz con padding')
+    
+    alto,ancho,canales = Matriz_rgb.shape
+    matriz_padding= np.zeros((alto + 2 , ancho + 2 , canales ),dtype=Matriz_rgb.dtype) 
+    matriz_padding[1:alto + 1, 1:ancho + 1, :] = Matriz_rgb 
+    
     return matriz_padding
 
 def aplicar_sobel(matriz: np.ndarray) -> np.ndarray:
@@ -68,27 +47,24 @@ def aplicar_sobel(matriz: np.ndarray) -> np.ndarray:
                         [0, 0, 0],
                         [-1, -2, -1]])
     
-    #llamo a la matriz con padding
 
-    #dimensiones de la matriz y kernels para aplicar el filtro sobel
+    
     alto,ancho = matriz.shape
     alto_kernel , ancho_kernel = sobel_x.shape
     
-    salida_alto = alto - alto_kernel
-    salida_ancho = ancho - ancho_kernel
+    salida_alto = alto - alto_kernel +1
+    salida_ancho = ancho - ancho_kernel +1
     
-    #matriz para guardar los resultados
+    
     matriz_salida_x = np.zeros((salida_alto,salida_ancho))
     matriz_salida_y = np.zeros((salida_alto,salida_ancho))
     
-    #convolucion para sobel x e y
+    
     for i in range(salida_alto):
         for a in range(salida_ancho):
             
-            #vecindad de cada pixel
             vecindad = matriz[i:i+ alto_kernel,a:a+ ancho_kernel]
             
-            #hacer suma y multiplicacion de kernels para cada sobel
             matriz_salida_x[i,a] = np.sum(vecindad * sobel_x)
             matriz_salida_y[i,a] = np.sum(vecindad * sobel_y)
     
@@ -101,35 +77,39 @@ def caclular_gradiente(sobel_x,sobel_y):
     
     return gradiente
 
-#quinta funcion crear matriz de energia
-def matriz_energia(matriz:np.ndarray)->np.ndarray:
-    """ 
-    Funcion que crea la matriz de energia 
-    PARAMETROS:
-        matriz:np.ndarray
-    RETORNA:
-        matriz:np.ndarray
-
-    """
-    sobel_y,sobel_x = aplicar_sobel(matriz)
-    energia = caclular_gradiente(sobel_x,sobel_y)
-    
-    return energia
 
 def matriz_energia_acumulada_columnas(energia:np.ndarray)->np.ndarray:
+    
     alto , ancho = energia.shape
-    matriz_energia_columnas=np.zeros((alto , ancho ))
-    energia_acumulada_columnas[0,:]=energia[0,:]
+    energia_acumulada=np.zeros((alto , ancho ))
+    energia_acumulada[0,:]=energia[0,:]
     
     for i in range (1, alto):
         for j in range (ancho):
             energia_acumulada[i,j]=energia[i,j] + min(
-                energia_acumulada_columnas[i-1, j], 
-                energia_acumulada_columnas[i-1 , j-1] if j >0 else np.inf,
-                energia_acumulada_columnas[i-1 , j+1] if j < ancho - 1 else np.inf
+                energia_acumulada[i-1, j], 
+                energia_acumulada[i-1 , j-1] if j >0 else np.inf,
+                energia_acumulada[i-1 , j+1] if j < ancho - 1 else np.inf
             )
+    
     return energia_acumulada
 
+
+def matriz_energia_acumulada_filas(energia:np.ndarray)->np.ndarray:
+    
+    alto , ancho = energia.shape
+    energia_acumulada=np.zeros((alto , ancho ))
+    energia_acumulada[0,:]=energia[0,:]
+    
+    for j in range (1, ancho):
+        for i in range (alto):
+            energia_acumulada[i,j]=energia[i,j] + min(
+                energia_acumulada[i, j-1], 
+                energia_acumulada[i-1 , j-1] if j >0 else np.inf,
+                energia_acumulada[i+1 , j-1] if j < alto - 1 else np.inf
+            )
+    
+    return energia_acumulada
 
 def costura_de_menor_energia_columnas (energia_acumulada:np.ndarray)-> np.ndarray:
     """ 
@@ -140,18 +120,21 @@ def costura_de_menor_energia_columnas (energia_acumulada:np.ndarray)-> np.ndarra
         costura_columnas: matriz con una columna formada por pixeles en cada fila 
     """
     alto , ancho =energia_acumulada.shape
-    costura=np.zeros(alto)
-    ultima_fila=energia_acumulada[:-1]
+    costura_columnas=np.zeros(alto , dtype=int)
+
+    ultima_fila=energia_acumulada[-1]
     valor_minimo=np.argmin(ultima_fila)
-    costura[-1]=valor_minimo
-    penultima_fila=ultima_fila -1
+    costura_columnas[-1]=valor_minimo
+    
     for i in range (alto-2 , -1 , -1):
-        j=costura[i+1]
-        costura[i]= j + np.argmin([
-            energia_acumulada[i , j],
-            energia_acumulada[i , j-1] if j > 0 else np.inf , 
-            energia_acumulada[i , j+1] if j < 0 else np.inf
-        ])-1
+        j=costura_columnas[i+1]
+        
+        izquierda = energia_acumulada[i, j - 1] if j > 0 else np.inf
+        centro = energia_acumulada[i , j]
+        derecha = energia_acumulada[i, j + 1] if j < ancho - 1 else np.inf
+
+        costura_columnas[i] = j +  np.argmin([izquierda,centro,derecha]) - 1
+    
     return costura_columnas
 
 def costura_de_menor_energia_filas (energia_acumulada:np.ndarray) -> np.ndarray:
@@ -162,22 +145,25 @@ def costura_de_menor_energia_filas (energia_acumulada:np.ndarray) -> np.ndarray:
     RETORNA:
         costura_filas: matriz con una fila por pixeles 
     """
-    alto , ancho =energia_acumulada.shape
-    costura=np.zeros(alto)
-    ultima_columna=energia_acumulada[:,-1]
-    valor_minimo=np.argmin(ultima_fila)
-    costura[:,-1]=valor_minimo
-    penultima_fila=ultima_columna -1
+    alto , ancho = energia_acumulada.shape
+    costura_filas = np.zeros(alto, dtype=int)
+
+    ultima_columna = energia_acumulada[:,-1]
+    valor_minimo = np.argmin(ultima_columna)
+    costura_filas[-1] = valor_minimo
+
     for j in range (ancho-2 , -1 , -1):
-        i=costura[j+1]
-        costura[j]= i + np.argmin([
-            energia_acumulada[i , j],
-            energia_acumulada[i-1 , j] if i > 0 else np.inf , 
-            energia_acumulada[i+1 , j] if i < 0 else np.inf
-        ])-1
+        i = costura_filas[j+1]
+        
+        arriba = energia_acumulada[i - 1, j] if i > 0 else np.inf
+        centro = energia_acumulada[i,j]
+        abajo =  energia_acumulada[i + 1, j] if i < alto - 1 else np.inf
+
+        costura_filas[i] = i + np.argmin([arriba,centro,abajo]) - 1
+
     return costura_filas
 
-def eliminar_costuras_filas (matriz_rgb , costura_columnas ):
+def eliminar_costuras_filas (matriz_rgb:np.ndarray , costura_columnas:np.ndarray ) -> np.ndarray:
     """
     Funcion que elimina las filas seleccionadas en la funcion anterior 
     PARAMETROS:
@@ -187,13 +173,16 @@ def eliminar_costuras_filas (matriz_rgb , costura_columnas ):
         nueva matriz: con una columna menos 
     """
     alto , ancho , canales= matriz_rgb.shape
-    nueva_matriz=np.zeros(alto, ancho - len(costura_columnas) , canales)
-    j=costura_columnas[i]
-    nueva_matriz[i]=np.delete(matriz_rgb , j , axis=0) #axis=0 filas #axis=1 columnas #axis=2 canales
+    nueva_matriz=np.zeros((alto-1, ancho , canales), dtype = np.uint8)
+    
+    for j in range(len(costura_columnas)):
+        y=costura_columnas[j]
+        nueva_matriz[:,j,:]=np.delete(matriz_rgb[:,j,:] , y , axis=0)
+
     return nueva_matriz
 
 
-def eliminar_costuras_columnas ( matriz_rgb , costura_filas):
+def eliminar_costuras_columnas ( matriz_rgb:np.ndarray , costura_filas:np.ndarray)-> np.ndarray:
     """
     Funcion que elimina las filas seleccionadas en la funcion anterior 
     PARAMETROS:
@@ -203,14 +192,24 @@ def eliminar_costuras_columnas ( matriz_rgb , costura_filas):
         nueva matriz:con una fila menos
     """
     alto , ancho , canales= matriz_rgb.shape
-    nueva_matriz=np.zeros(alto-len(costura_filas), ancho, canales)
-    i=costura_filas[j]
-    nueva_matriz[j]=np.delete(matriz_rgb , i , axis=1) #axis=0=filas #axis=1=columnas #axis=2=canales
+    nueva_matriz=np.zeros((alto, ancho-1, canales), dtype=np.uint8)
+    
+    for i in range(len(costura_filas)):
+        x=costura_filas[i]
+        nueva_matriz[i]=np.delete(matriz_rgb[i] , x , axis=0) 
+    
     return nueva_matriz
 
-def mostrar_imagen_final (imagen):
-    imagen = (imagen - imagen.min()) /(imagen.max() - imagen.min()) * 255
-    imagen = imagen.astype(np.uint8)
-    imagen_final=Image.fromarray(imagen)
-    imagen_final.show(title="Imagen final de la matriz original")
+def mostrar_imagen_final(imagen):
+    if imagen.size == 0:
+        return'La imagen esta vacia'
+    imagen = (imagen - imagen.min()) / (imagen.max() - imagen.min()) * 255
+    imagen_ = imagen.astype(np.uint8)
+    
+    imagen_final = Image.fromarray(imagen_)
+    imagen_final.show()
 
+
+def callback(imagen_visible,imagen):
+    imagen_visible.config(image=imagen)
+    imagen_visible.image=imagen
